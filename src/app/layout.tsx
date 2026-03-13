7b241a2ee6ca99944d47848f161880d45673d2fc
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2026-03-13 05:40:44 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono, Rubik, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ToastProvider } from "@/components/ToastProvider";
 import { NotificationProvider } from "@/hooks/use-notifications";
@@ -17,13 +18,13 @@ const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
 });
 
-// Rubik font (global)
+// Rubik font
 const rubik = Rubik({
   subsets: ["latin", "arabic"],
   variable: "--font-rubik",
 });
 
-// Outfit font for headers
+// Outfit for headers
 const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-outfit",
@@ -34,32 +35,37 @@ export const metadata: Metadata = {
   description:
     "Master your future skills with Chameleon, the ultimate platform for learning and growth With a focus on technology, design, and innovation.",
   icons: {
-    icon: [
-      { url: "/images/1212-removebg-preview.png", sizes: "any" },
-    ],
+    icon: [{ url: "/images/1212-removebg-preview.png", sizes: "any" }],
     apple: "/images/1212-removebg-preview.png",
   },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5932974277970825"
+          crossOrigin="anonymous"
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} ${outfit.variable} antialiased`}
-      >      
-        
-        {/* لف الـ children جوه NotificationProvider وبعدها ToastProvider */}
+      >
         <NotificationProvider>
-          <ToastProvider>{children}</ToastProvider>
+          <ToastProvider>
+            <DevToolsProtection />
+            {children}
+          </ToastProvider>
         </NotificationProvider>
       </body>
-       <script async         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5932974277970825"
-     crossorigin="anonymous">
-     </script>
     </html>
   );
 }
