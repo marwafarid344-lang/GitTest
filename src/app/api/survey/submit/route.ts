7a24e1541852a3932554d751dcb9ab7e3c9bd702
Abdo Ahmed <163312670+AbdoAhmedAbdelmonem@users.noreply.github.com>
2026-03-18 17:19:26 +0200

    Update Google Forms URL in survey submission

--- src/app/api/survey/submit/route.ts
+++ src/app/api/survey/submit/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.text()
     const res = await fetch(
-      "https://docs.google.com/forms/d/e/1FAIpQLSccLnBUzkM_m-vynoPJCK7YQm6I1UrHDOwpfBSY13itquf5hw/formResponse",
+      "https://docs.google.com/forms/d/e/1FAIpQLSd-mzQXe4BIt6YYbqO-U7IOnjCyQ1dmY1P1k7s6vhacdfLxIw/formResponse",
       {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
